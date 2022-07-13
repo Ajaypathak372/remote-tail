f85e3105735053942d0c0d67c08d2de7b9638109
Ajay Pathak <ajaypathak372@gmail.com>
2022-07-13 13:21:20 +0530

    Code updated

--- agent/agent.py
+++ agent/agent.py
@@ -1,39 +1,52 @@
 #!/usr/bin/python3
-# This runs on the computer(s) you want to read the file from
-# Make sure to change out the HOST and PORT variables
-HOST = '0.0.0.0'
-PORT = 8000
 
 from xmlrpc.server import SimpleXMLRPCServer
 from xmlrpc.server import SimpleXMLRPCRequestHandler
+import argparse
 import os
+
+def get_args():
+    parser = argparse.ArgumentParser(description="Tail logs from remote server")
+
+    parser.add_argument("-host", "--hostname",
+                  dest="hostname",
+                  default="127.0.0.1",
+                  help="Server Host, defaults to 127.0.0.1")
+    parser.add_argument("-p", "--port",
+                  dest="port",
+                  default=8000,
+                  type=int,
+                  help="Server Port number, defaults to 8000")
+
+    return parser.parse_args()
 
 class RequestHandler(SimpleXMLRPCRequestHandler):
     rpc_paths = ('/RPC2')
 
 def GetSize(filename):
-   # get file size
-   return os.stat(filename)[6]
+    # get file size
+    return os.stat(filename)[6]
 
 def tail(filename, seek):
-   #Set the filename and open the file
-   f = open(filename,'r')
+    #Set the filename and open the file
+    f = open(filename,'r')
 
-   #Find the size of the file and move to the end
-   f.seek(seek)
-   return f.read()
+    #Find the size of the file and move to the end
+    f.seek(seek)
+    return f.read()
 
 def CreateServer():
-   # Create server
-   server = SimpleXMLRPCServer((HOST, PORT),
+    args = get_args()
+    # Create server
+    server = SimpleXMLRPCServer((args.hostname, args.port),
                                requestHandler=RequestHandler)
 
-# register functions
-   server.register_function(tail, 'tail')
-   server.register_function(GetSize, 'GetSize')
+    # register functions
+    server.register_function(tail, 'tail')
+    server.register_function(GetSize, 'GetSize')
 
-   # Run the server's main loop
-   server.serve_forever()
+    # Run the server's main loop
+    server.serve_forever()
 
 # start server
 CreateServer()
--- client/client.py
+++ client/client.py
@@ -1,74 +1,71 @@
 #!/usr/bin/python3
-# This should be run on the computer you want to output the files
-# You must pass a filename and a location
-# filename must be the full path from the root directory, or relative path
-# from the directory the server is running
 
-import time, sys
-import argparse
 from xmlrpc import client
+import time
+import sys
+import argparse
 
 parser = argparse.ArgumentParser(description="Remote Tail logs")
 
 def get_args():
-   parser = argparse.ArgumentParser(description="Tail logs from remote server")
+    parser = argparse.ArgumentParser(description="Tail logs from remote server")
 
-   parser.add_argument("-host", "--hostname", 
+    parser.add_argument("-host", "--hostname",
                   dest="hostname",
                   required=True,
                   help="Host IP Address")
-   parser.add_argument("-f", "--filepath", 
+    parser.add_argument("-f", "--filepath",
                   dest="filepath",
-                  required=True, 
+                  required=True,
                   help="Path where the file is located, for example /var/log/syslog")
-   parser.add_argument("-p", "--port", 
-                  dest="port", 
-                  default="8000", 
+    parser.add_argument("-p", "--port",
+                  dest="port",
+                  default="8000",
                   help="Agent Port number, defaults to 8000")
-   
-   return parser.parse_args()
+
+    return parser.parse_args()
 
 def tail(filename, hostname, port):
-   # connect to server
-   location = "http://" + hostname + ":" + port
-   s = client.ServerProxy(location)
+    # connect to server
+    location = "http://" + hostname + ":" + port
+    s = client.ServerProxy(location)
 
-   # get starting length of file
-   curSeek = s.GetSize(filename)
+    # get starting length of file
+    curSeek = s.GetSize(filename)
 
-   # constantly check
-   while True:
-      time.sleep(1) # make sure to sleep
+    # constantly check
+    while True:
+        time.sleep(1) # make sure to sleep
 
-      # get a new length of file and check for changes
-      prevSeek = curSeek
+        # get a new length of file and check for changes
+        prevSeek = curSeek
 
-      # some times it fails if the file is being writter to,
-      # we'll wait another second for it to finish
-      try:
-         curSeek = s.GetSize(filename)
-      except:
-         pass
+        # some times it fails if the file is being writter to,
+        # we'll wait another second for it to finish
+        try:
+            curSeek = s.GetSize(filename)
+        except:
+            pass
 
-      # if file length has changed print it
-      if prevSeek != curSeek:
-         print(s.tail(filename, prevSeek))
+        # if file length has changed print it
+        if prevSeek != curSeek:
+            print(s.tail(filename, prevSeek))
 
 def main():
 
-   args = get_args()
+    args = get_args()
 
-   try:
-      tail(args.filepath, args.hostname, args.port)
-   except ConnectionError:
-      print(f"[ERROR]: Connection refused, failed to connect to agent at {args.hostname}")
-      sys.exit(111)
-   except client.Fault as err:
-      if err.faultString.find('FileNotFoundError'):
-         print("[ERROR]: File not found, please enter a valid file path")
-         sys.exit(2)
-      else:
-         print(err)
-         sys.exit(1)
+    try:
+        tail(args.filepath, args.hostname, args.port)
+    except ConnectionError:
+        print(f"[ERROR]: Connection refused, failed to connect to agent at {args.hostname}")
+        sys.exit(111)
+    except client.Fault as err:
+        if err.faultString.find('FileNotFoundError'):
+            print("[ERROR]: File not found, please enter a valid file path")
+            sys.exit(2)
+        else:
+            print(err)
+            sys.exit(1)
 
 main()
